55ee64aeb473d80b1d140ae68574d59bef35f049
John Marzulli <john.marzulli@hotmail.com>
2026-06-08 19:34:39 -0700

    Add ability to return the name of the current status.

--- busylight_nano/statusLight.h
+++ busylight_nano/statusLight.h
@@ -3,8 +3,7 @@
 
 #include <Adafruit_NeoPixel.h>
 
-enum Status
-{
+enum Status {
   Off = 0,
   Available = 1,
   Caution = 2,
@@ -13,145 +12,127 @@ enum Status
   Fun = 5
 };
 
-class StatusLight
-{
+class StatusLight {
 public:
-  StatusLight(
-      uint16_t ledCount,
-      uint16_t hardwarePin)
-  {
+  StatusLight(uint16_t ledCount, uint16_t hardwarePin) {
     strip = Adafruit_NeoPixel(ledCount, hardwarePin, NEO_GRB + NEO_KHZ800);
-    currentMode = Available;
+    currentMode = Busy;
   }
 
-  void initialize()
-  {
+  void initialize() {
     strip.begin();
     strip.setBrightness(50);
     strip.show();
   }
 
   void setMode(
-      char cmd)
-  {
-    if (cmd == '0' || cmd == 0x30) // Off
+    char cmd) {
+    if (cmd == '0' || cmd == 0x30)  // Off
       setOff();
-    if (cmd == '1' || cmd == 0x31) // Available
+    if (cmd == '1' || cmd == 0x31)  // Available
       setAvailable();
-    if (cmd == '2' || cmd == 0x32) // Caution
+    if (cmd == '2' || cmd == 0x32)  // Caution
       setCaution();
-    if (cmd == '3' || cmd == 0x33) // Busy
+    if (cmd == '3' || cmd == 0x33)  // Busy
       setBusy();
-    if (cmd == '4' || cmd == 0x34) // Klaxon Busy
+    if (cmd == '4' || cmd == 0x34)  // Klaxon Busy
       setSuperBusy();
-    if (cmd == '5' || cmd == 0x35) // Disco Fun
+    if (cmd == '5' || cmd == 0x35)  // Disco Fun
       setFun();
   }
 
-  void setOff()
-  {
+  const char *getModeName() const {
+    switch (currentMode) {
+      case Off: return "Off";
+      case Available: return "Available";
+      case Caution: return "Caution";
+      case Busy: return "Busy";
+      case SuperBusy: return "Super Busy";
+      case Fun: return "Fun";
+      default: return "Unknown";
+    }
+  }
+
+  void setOff() {
     currentMode = Off;
   }
 
-  void setAvailable()
-  {
+  void setAvailable() {
     currentMode = Available;
   }
 
-  void setCaution()
-  {
+  void setCaution() {
     currentMode = Caution;
   }
 
-  void setBusy()
-  {
+  void setBusy() {
     currentMode = Busy;
   }
 
-  void setSuperBusy()
-  {
+  void setSuperBusy() {
     currentMode = SuperBusy;
   }
 
-  void setFun()
-  {
+  void setFun() {
     currentMode = Fun;
   }
 
-  void Service()
-  {
+  void Service() {
     unsigned long now = millis();
-    switch (currentMode)
-    {
-    case Off:
-      offLoop(now);
-      break;
-    case Available:
-      availableLoop(now);
-      break;
-    case Caution:
-      cautionLoop(now);
-      break;
-    case Busy:
-      busyLoop(now);
-      break;
-    case SuperBusy:
-      superBusyLoop(now);
-      break;
-    case Fun:
-    default:
-      funLoop(now);
-      break;
+    switch (currentMode) {
+      case Off:
+        offLoop(now);
+        break;
+      case Available:
+        availableLoop(now);
+        break;
+      case Caution:
+        cautionLoop(now);
+        break;
+      case Busy:
+        busyLoop(now);
+        break;
+      case SuperBusy:
+        superBusyLoop(now);
+        break;
+      case Fun:
+      default:
+        funLoop(now);
+        break;
     }
   }
 
 private:
-  void offLoop(
-      unsigned long now)
-  {
-    if (now - lastUpdate >= 500)
-    {
+  void offLoop(unsigned long now) {
+    if (now - lastUpdate >= 500) {
       strip.fill(strip.Color(0, 0, 0));
       strip.show();
       lastUpdate = now;
     }
   }
 
-  void availableLoop(
-      unsigned long now)
-  {
+  void availableLoop(unsigned long now) {
     singleColorLoop(now, strip.Color(0, 255, 0));
   }
 
-  void cautionLoop(
-      unsigned long now)
-  {
+  void cautionLoop(unsigned long now) {
     singleColorLoop(now, strip.Color(255, 255, 0));
   }
 
-  void busyLoop(
-      unsigned long now)
-  {
+  void busyLoop(unsigned long now) {
     singleColorLoop(now, strip.Color(255, 0, 0));
   }
 
-  void singleColorLoop(
-      unsigned long now,
-      uint32_t color)
-  {
-    if (now - lastUpdate >= 500)
-    {
+  void singleColorLoop(unsigned long now, uint32_t color) {
+    if (now - lastUpdate >= 500) {
       strip.fill(color);
       strip.show();
       lastUpdate = now;
     }
   }
 
-  void superBusyLoop(
-      unsigned long now)
-  {
-    if (now - lastUpdate >= 100)
-    {
+  void superBusyLoop(unsigned long now) {
+    if (now - lastUpdate >= 100) {
       ++currentLed;
       if (currentLed >= strip.numPixels())
         currentLed = 1;
@@ -168,13 +149,9 @@ private:
     }
   }
 
-  void funLoop(
-      unsigned long now)
-  {
-    if (now - lastUpdate >= 10)
-    {
-      for (uint16_t i = 0; i < strip.numPixels(); i++)
-      {
+  void funLoop(unsigned long now) {
+    if (now - lastUpdate >= 10) {
+      for (uint16_t i = 0; i < strip.numPixels(); i++) {
         strip.setPixelColor(i, getWheel((i + rainbowStep) & 255));
       }
       strip.show();
@@ -183,20 +160,13 @@ private:
     }
   }
 
-  uint32_t getWheel(
-      byte WheelPos)
-  {
-    if (WheelPos < 85)
-    {
+  uint32_t getWheel(byte WheelPos) {
+    if (WheelPos < 85) {
       return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-    }
-    else if (WheelPos < 170)
-    {
+    } else if (WheelPos < 170) {
       WheelPos -= 85;
       return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-    }
-    else
-    {
+    } else {
       WheelPos -= 170;
       return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
     }
@@ -209,4 +179,4 @@ private:
   uint16_t rainbowStep = 0;
 };
 
-#endif // STATUS_LIGHT_H
+#endif  // STATUS_LIGHT_H
